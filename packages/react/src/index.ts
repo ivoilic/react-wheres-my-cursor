48076580ffa8766fc8788c52278454194ea42929
Ivo Ilić <3476570+ivoilic@users.noreply.github.com>
2022-10-30 22:28:56 -0400

    Refactored to use shared event listeners and global state

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,39 +1,49 @@
 import { useEffect, useCallback, useState, RefObject } from 'react';
+import { proxy, useSnapshot, subscribe } from 'valtio';
 
+// Shared State
+const state = proxy({ cursorPosition: { x: 0, y: 0 }, i: 1 });
+
+//Shared Event Handlers
+const handleResize = () => {
+  ++state.i;
+};
+const handleMouseMove = (e: MouseEvent) => {
+  state.cursorPosition = { x: e.clientX, y: e.clientY };
+};
+
+// Shared Event Listeners
+if (typeof window != 'undefined') {
+  window.addEventListener('mousemove', handleMouseMove);
+  window.addEventListener('resize', handleResize);
+  window.addEventListener('scroll', handleResize);
+}
+
+/**
+ * Get the relative angle and position of the cursor
+ * @param ref
+ * @returns
+ */
 export const useWheresMyCursor = (ref: RefObject<HTMLElement | null>) => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [targetPosition, setTargetPosition] = useState({ x: 0, y: 0 });
+  const { cursorPosition, i } = useSnapshot(state);
+  let targetPosition = { x: 0, y: 0 };
 
-  const handleMouseMove = useCallback((e: MouseEvent) => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
-  }, []);
+  if (ref?.current != undefined) {
+    const rect = ref?.current.getBoundingClientRect();
+    targetPosition = { x: rect.x + rect.width * 0.5, y: rect.y + rect.height * 0.5 };
+  }
 
-  const handleResize = useCallback(() => {
-    const target = ref?.current;
-    if (target == undefined) return;
-    const rect = target.getBoundingClientRect();
-    setTargetPosition({ x: rect.x + rect.width * 0.5, y: rect.y + rect.height * 0.5 });
-  }, [ref]);
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('scroll', handleResize);
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      window.addEventListener('resize', handleResize);
-      window.addEventListener('scroll', handleResize);
-      window.addEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  const offset = { x: targetPosition.x - mousePosition.x, y: targetPosition.y - mousePosition.y };
+  const positionDelta = {
+    x: targetPosition.x - cursorPosition.x,
+    y: targetPosition.y - cursorPosition.y,
+  };
   const radians =
-    Math.atan2(mousePosition.x - targetPosition.x, mousePosition.y - targetPosition.y) * -1;
+    Math.atan2(cursorPosition.x - targetPosition.x, cursorPosition.y - targetPosition.y) * -1;
   const degrees = radians * (180 / Math.PI);
+
   return {
-    offset,
-    cursorPosition: mousePosition,
+    positionDelta,
+    cursorPosition,
     targetPosition,
     angle: {
       degrees,
